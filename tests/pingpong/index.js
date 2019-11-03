c78661075190aeed3a582ece170ede576d78a3c1
Marcus <marcus@mandworks.com>
2019-11-03 11:55:29 -0500

    Add logo, simplify examples, start real testing

--- tests/pingpong/index.js
+++ tests/pingpong/index.js
@@ -1,42 +1,18 @@
 const {Collexion, Link, LinkArray} = require('./../../core/Collexion.js');
 const PingPong = require('./pingpong.js')
 
-;(async () => {
-	const collexion = new Collexion({
-		A: {
-			Code: PingPong,
-			Data: {
-				start: true,
-				dest: 'B'
-			}
-		},
-		B: {
-			Code: PingPong,
-			Data: {
-				dest: 'A'
-			}
+const collexion = new Collexion({
+	A: {
+		Code: PingPong,
+		Data: {
+			start: true,
+			dest: 'B'
 		}
-	});
-})();
-
-
-// const ExampleModule = require('./module.js.js');
-
-// module.exports = {
-// 	Entities: {
-// 		A: {
-// 			Code: ExampleModule,
-// 			Data: {
-// 				thing: ref('B'),
-// 				boop: true
-// 			}
-// 		},
-// 		B: {
-// 			Code: ExampleModule,
-// 			Data: {
-// 				thing: ref('B')
-// 			}
-// 		}
-// 	}
-// }
-
+	},
+	B: {
+		Code: PingPong,
+		Data: {
+			dest: 'A'
+		}
+	}
+});
